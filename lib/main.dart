ca5f5fa4d7e4f188d58d1e03f3f000d3cfc0056c
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-09 09:09:35 +0300

    add firebase dependancy for android

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
 import 'package:insta_clone/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
